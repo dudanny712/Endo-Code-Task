647f1c54c63e08cec603f4140f8ebf54254b1848
dudanny712 <58696798+dudanny712@users.noreply.github.com>
2022-05-02 12:34:46 +0200

    added versionz end point

--- src/pages/version/index.js
+++ src/pages/version/index.js
@@ -1,20 +1,21 @@
 import React from 'react'
-import Data from './data.json'
+import GitInfo from 'react-git-info/macro';
+
+
+const PACKAGE_VERSION = require("../../../package.json").version;
+const PACKAGE_NAME = require("../../../package.json").name;
+const gitInfo = GitInfo();
+
 const Version = () => {
   return (
     <div>
-      {
-          Data.map(post => {
-              return (
-                  <>
-                    <h1>{post.name}</h1>
-                    <h1>{post.version}</h1>
-                  </>
-                  
-              )
-          })
-      }
+        <h1>Name: {PACKAGE_NAME}</h1>
+       <h1>Version: {PACKAGE_VERSION}</h1>
+       <h1> Git Hash: {gitInfo.commit.hash}</h1>
+       
+      
     </div>
+    
   )
 }
 
